2a299b5505d8bf0f043ae3c0e32e46ee25fdfef7
sigma4code <sigma4code@gmail.com>
2025-06-17 22:53:50 +0300

    add /model and add model api

--- api/index.py
+++ api/index.py
@@ -47,7 +47,7 @@ def predict():
         "probabilities": manual_prediction_proba[0].tolist()
     })
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 
